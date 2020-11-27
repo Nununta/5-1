375d1506a3443308493d6ced4b0cdb5bd1575fde
“YutaYuki” <yuta@example.com>
2020-11-28 07:51:44 +0900

    11/28

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,13 +1,13 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-
+use App\User;
 use App\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        'body' => $faker->sentences,
+        'body' => $faker->paragraph,
         'user_id' => function () {
             return factory(App\User::class)->create()->id;
         }
